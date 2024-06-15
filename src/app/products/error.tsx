d463fac3f306d39092804f1d17c93fbe4cba79fa
Rakesh <rakeshgandla200@gmail.com>
2024-06-15 23:04:01 +0530

    error handling-2

--- src/app/products/error.tsx
+++ src/app/products/error.tsx
@@ -1,4 +1,15 @@
 "use client"
-export default function ErrorBoundary() {
-    return <h2>Error in Products</h2>
+export default function ErrorBoundary({
+    error,
+    reset,
+}: {
+    error: Error
+    reset: () => void
+}) {
+    return (
+        <>
+            <h2>Error in Products {error.message}</h2>
+            <button onClick={reset}>Reset Error</button>
+        </>
+    )
 }
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -7,11 +7,16 @@ export default function Products() {
     const clickHandler = () => {
         router.push("/")
     }
+    if (Math.floor(Math.random() * 2) == 1) {
+        throw    new Error("Error in products")
+    }
     // throw new Error("test error")
     return (
         <div className="flex flex-col gap-3">
             <Link href="/">Back to Home</Link>
-            <button className="btn-primary" onClick={clickHandler}>Back to Home</button>
+            <button className="btn-primary" onClick={clickHandler}>
+                Back to Home
+            </button>
             <h1>Products</h1>
             <h3>
                 <Link href="./products/1">Product 1</Link>
@@ -20,7 +25,9 @@ export default function Products() {
                 <Link href="./products/2">Product 2</Link>
             </h3>
             <h3>
-                <Link href="./products/3" replace>Product 3</Link>
+                <Link href="./products/3" replace>
+                    Product 3
+                </Link>
             </h3>
         </div>
     )
