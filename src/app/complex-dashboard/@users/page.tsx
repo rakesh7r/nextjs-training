6d6e3b9888674908439fb89806e6c7b9e9cdd56f
Rakesh <rakeshgandla200@gmail.com>
2024-06-17 16:52:37 +0530

    parallel routes

---
+++ src/app/complex-dashboard/@users/page.tsx
@@ -0,0 +1,3 @@
+export default function UserAnalytics() {
+    return <div>User Analytics</div>
+}
---
+++ src/app/complex-dashboard/layout.tsx
@@ -0,0 +1,26 @@
+import Card from "../components/Card"
+
+export default function ComplexLayout({
+    children,
+    notifications,
+    users,
+    revenue,
+}: {
+    children: React.ReactNode
+    notifications: React.ReactNode
+    users: React.ReactNode
+    revenue: React.ReactNode
+}) {
+    return (
+        <div>
+            <Card>{children}</Card>
+            <div className="flex flex-rosw">
+                <div className="w-[50vw]">
+                    <Card>{users}</Card>
+                    <Card>{revenue}</Card>
+                </div>
+                <Card>{notifications}</Card>
+            </div>
+        </div>
+    )
+}
