1d4db56ac6e8bab5b75eba11c90be54a2bb47846
Rakesh Gandla <41822443+rakesh7r@users.noreply.github.com>
2024-06-18 11:02:58 +0000

    parallel routes

---
+++ src/app/complex-dashboard/@notifications/archived/page.tsx
@@ -0,0 +1,8 @@
+import Link from "next/link";
+
+export default function ArchivedNotifications() {
+    return <div>
+        Archived Notifications
+        <Link href="/complex-dashboard/">Default</Link>
+    </div>
+}
--- src/app/complex-dashboard/@notifications/page.tsx
+++ src/app/complex-dashboard/@notifications/page.tsx
@@ -1,3 +1,7 @@
+import Link from "next/link";
+
 export default function Notifications() {
-    return <div>Notifications</div>
+    return <div className="h-[50vh]">Notifications
+    <Link href="/complex-dashboard/archived"> Archived</Link>
+    </div>
 }
