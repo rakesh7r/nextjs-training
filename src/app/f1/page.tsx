1a6f02bc47e65f1762b4534145ce63e4ef4654df
Rakesh <rakeshgandla200@gmail.com>
2024-07-06 12:48:02 +0530

    intercepted routes

---
+++ src/app/f1/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link"
+
+export default function Page() {
+    return (
+        <>
+            <h1>f1 Page</h1>
+            <Link href="/f2">F2</Link>
+        </>
+    )
+}
